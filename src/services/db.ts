fa577188e815cdd307537ce655232bf324aa3917
Ruiz <alexander.rivail@gmail.com>
2022-08-02 02:26:14 +0100

    port db

--- src/services/db.ts
+++ src/services/db.ts
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 //DB
 const pool = new Pool({
     user: 'ruiz',
-    host: 'localhost',
+    host: '127.0.0.1',
     database: 'cosmobots',
     password: 'q1w2e3',
     port: 5432,
